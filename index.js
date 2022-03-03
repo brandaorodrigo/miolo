061861f3b5af8f9386fb46a28e532f1483df8506
= <=>
2022-03-03 03:55:10 -0300

    ground

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-let player;
-let cursors;
-let cam;
-
 class MyGame extends Phaser.Scene {
     constructor() {
         super();
-        this.maxSpeed = 1000;
+        this.maxSpeed = 600;
         this.currentSpeed = 0;
         this.jumping = false;
     }
@@ -17,41 +13,51 @@ class MyGame extends Phaser.Scene {
         this.load.image('tiles', 'assets/map.png');
         this.load.tilemapTiledJSON('map', 'assets/map.json');
 
+        this.load.image('tiles2', 'assets/ground.png');
+        this.load.tilemapTiledJSON('map2', 'assets/ground.json');
+
         this.load.spritesheet('player', 'assets/rod.png', {
-            frameWidth: 48,
-            frameHeight: 48,
+            frameWidth: 32,
+            frameHeight: 32,
         });
     }
 
+    render() {
+        game.debug.cameraInfo(game.camera, 32, 32);
+        game.debug.spriteCoords(player, 32, 500);
+    }
+
     create() {
-        //const back = this.add.image(0, 0, 'back').setScale(3);
-        this.back = this.add
-            .tileSprite(0, 0, 3000, 600, 'back')
-            .setScale(3)
-            .setOrigin(0, 0);
+        this.add
+            .tileSprite(0, 16, 10000, 240, 'back')
+            .setOrigin(0, 0)
+            .setScrollFactor(0.2);
 
-        const map = this.make.tilemap({ key: 'map' });
+        const map = this.make.tilemap({ key: 'map2' });
 
-        const tileset = map.addTilesetImage('map', 'tiles');
-        const ground = map.createLayer('chao', tileset, 0, 0);
+        const tileset = map.addTilesetImage('ground', 'tiles2');
+        const ground = map.createLayer('ground', tileset, 0, 0);
         //const collider = map.createLayer('colider', tileset, 0, 0);
+
         //const fundo = map.createLayer('fundo', tileset, 0, 0);
-        ground.setCollisionByProperty({ collider: true });
         //fundo.setDepth(10);
+
+        ground.setCollisionByProperty({ collider: true });
 
         this.physics.world.setFPS(44);
         this.physics.world.bounds.width = ground.width;
         this.physics.world.bounds.height = ground.height;
 
-        this.player = this.physics.add.sprite(100, 300, 'player');
-        this.player.setScale(3).setOrigin(0, 0);
+        this.player = this.physics.add.sprite(40, 240, 'player');
+        //this.player.body.bounce.set(0.3);
         this.player.setCollideWorldBounds(true);
 
         //collider.setCollisionByProperty({ collider: true });
         //this.physics.add.collider(this.player, collider);
         this.physics.add.collider(this.player, ground);
 
-        this.cameras.main.startFollow(this.player);
+        this.cameras.main.startFollow(this.player, false, 1, 1, -100);
+
         this.cameras.main.setBounds(
             0,
             0,
@@ -59,25 +65,26 @@ class MyGame extends Phaser.Scene {
             ground.layer.heightInPixels,
             true
         );
-    }
 
-    update() {
         // configura controles
-        cursors = this.input.keyboard.addKeys({
+        this.cursors = this.input.keyboard.addKeys({
             down: Phaser.Input.Keyboard.KeyCodes.S,
             left: Phaser.Input.Keyboard.KeyCodes.A,
             right: Phaser.Input.Keyboard.KeyCodes.D,
             jump: Phaser.Input.Keyboard.KeyCodes.W,
             replay: Phaser.Input.Keyboard.KeyCodes.R,
         });
+    }
 
-        // acelera skate até 1000 se para de apertar vai freiando em -0.2
-        if (cursors.right.isDown) {
+    update_player() {
+        if (this.cursors.right.isDown) {
             if (this.currentSpeed < this.maxSpeed) {
-                this.currentSpeed += 4;
+                this.currentSpeed += 1;
                 this.player.setVelocityX(this.currentSpeed);
             }
-        } else {
+        }
+
+        if (this.cursors.right.isUp) {
             if (this.currentSpeed > 0) {
                 this.currentSpeed -= 0.5;
                 this.player.setVelocityX(this.currentSpeed);
@@ -86,60 +93,66 @@ class MyGame extends Phaser.Scene {
             }
         }
 
-        // se aperta para trás o skate freia em -0.6
-        if (cursors.left.isDown && this.currentSpeed !== 0) {
-            this.currentSpeed -= 1.4;
-            if (this.currentSpeed < 0) this.currentSpeed = 0;
+        if (this.cursors.left.isDown) {
+            if (this.currentSpeed !== 0) {
+                this.currentSpeed -= 2;
+                if (this.currentSpeed < 0) {
+                    this.currentSpeed = 0;
+                }
+            }
         }
 
-        // o pulo acontece somente um de cada vez em velocidade -500
-        if (
-            cursors.jump.isDown &&
-            !this.jumping &&
-            this.player.body.onFloor()
-        ) {
-            this.jumping = true;
-            this.player.body.setVelocityY(
-                this.currentSpeed > 400 ? -600 : -300
-            );
+        if (this.cursors.jump.isDown) {
+            if (!this.jumping && this.player.body.onFloor()) {
+                this.jumping = true;
+                this.player.body.setVelocityY(
+                    this.currentSpeed > 200 ? -400 : -250
+                );
+            }
         }
 
-        // libera pulo de novo depois que o anterior terminou
-        if (cursors.jump.isUp && this.jumping && this.player.body.onFloor()) {
-            this.jumping = false;
+        if (this.cursors.jump.isUp) {
+            if (this.jumping && this.player.body.onFloor()) {
+                this.jumping = false;
+            }
         }
 
-        // abaixar
-        if (cursors.down.isDown) {
-            var f = this.player.frame;
-            this.player.body.setSize(30, 30, false).setOffset(f.x, f.y + 16);
-        } else {
-            var f = this.player.frame;
-            this.player.body.setSize(30, 46, false).setOffset(f.x, f.y);
+        if (this.cursors.down.isDown) {
+            this.player.body
+                .setSize(30, 30, false)
+                .setOffset(this.player.frame.x, this.player.frame.y + 16);
         }
 
-        // replay
-        if (cursors.replay.isDown) {
+        if (this.cursors.down.isUp) {
+            this.player.body
+                .setSize(30, 46, false)
+                .setOffset(this.player.frame.x, this.player.frame.y);
+        }
+
+        if (this.cursors.replay.isDown) {
             this.currentSpeed = 0;
             this.player.setVelocityX(-4000);
         }
+    }
+
+    update() {
+        this.update_player();
     }
 }
 
 const config = {
     type: Phaser.CANVAS,
-    parent: 'phaser-example',
-    width: 1280,
-    frames: 12,
-    height: 720,
-    backgroundColor: '0xffffff',
-    scene: MyGame,
     pixelArt: true,
+    width: 384,
+    height: 240,
+    zoom: 2,
+    backgroundColor: '0xffffff',
+    parent: 'game',
+    scene: MyGame,
     physics: {
         default: 'arcade',
         arcade: {
             gravity: { y: 1000 },
-            bounce: 0.5,
             debug: true,
         },
     },
